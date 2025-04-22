80afe7fe2a80ddf95abe1e6f260ced0c7478153a
FileX <frag.mich.hier@gmail.com>
2025-04-22 18:30:02 +0200

    Add Mycorrhiza class template to represent symbiotic relationship between fungus and plant roots

---
+++ VirtualArboretum/Core/Domain/Entities/Mycorrhiza.cs
@@ -0,0 +1,12 @@
+﻿namespace VirtualArboretum.Core.Domain.Entities;
+
+
+/// <summary>
+/// A Mycorrhiza is a symbiotic Mechanism between a fungus and a plants root system. <br/>
+/// Does provide nutritional access to all plants connected to the Mycelium and its Hyphae.
+/// </summary>
+public class Mycorrhiza // : Hypha  // Might be more appropriate to instantiate a Hypha containing a Mycorrhiza..
+{
+    private int _dummy = 0;
+
+}
