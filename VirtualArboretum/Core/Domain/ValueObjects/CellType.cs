9dc16cac4784e2266fffe729610ab3d169528a27
FileX <frag.mich.hier@gmail.com>
2025-04-22 21:55:22 +0200

    Enhance CellType constructor to log warnings for unsupported content types

--- VirtualArboretum/Core/Domain/ValueObjects/CellType.cs
+++ VirtualArboretum/Core/Domain/ValueObjects/CellType.cs
@@ -5,12 +5,21 @@ namespace VirtualArboretum.Core.Domain.ValueObjects;
 public class CellType : ContentType
 {
     public CellType(string name) : base(name)
-    { }
+    {
+        if (!IsOfficiallySupported())
+        {
+            Console.Error.WriteLine(
+                $"Warning: {name} is not an officially supported content type, " +
+                $"will be handled as raw octet-stream."
+            );
+        }
+    }
 
     public bool IsOfficiallySupported() => this.Name switch
     {
         "application/json" => true,
         "application/octet-stream" => true,
+        "application/ld+json" => false,  // TODO: Once Core is stable.
         _ => false
     };
 }
