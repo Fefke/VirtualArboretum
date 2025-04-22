1f4cd81f872e67ca85c3dbc1c2dff6dcf5c0ba6f
FileX <frag.mich.hier@gmail.com>
2025-04-22 21:56:29 +0200

    Refactor Plant class to use HyphaeStrain for Name and update associated methods for better type consistency

--- VirtualArboretum/Core/Domain/Entities/Plant.cs
+++ VirtualArboretum/Core/Domain/Entities/Plant.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections.Immutable;
-using VirtualArboretum.Core.Domain.Services;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using VirtualArboretum.Core.Domain.AggregateRoots;
 using VirtualArboretum.Core.Domain.ValueObjects;
 
 namespace VirtualArboretum.Core.Domain.Entities;
 
 public class Plant
 {
-    
-    public HyphaApex Name
+
+    public HyphaeStrain Name
     {
-        // may be random name or a name & mutable
+        // i.R. same as primary location of the plant in filesystem
+        // ...relative to garden its placed in.
         get; init;
     }
+
     public readonly Fingerprint UniqueMarker;
 
-
-    public Dictionary<string, Hypha>? AssociatedHyphae;
+    public List<HyphaeStrain> AssociatedHyphae;
     // can only be set by providing Hypha, which is being serialized for key.
 
     public ImmutableSortedDictionary<Fingerprint, Cell> Cells
@@ -26,35 +29,102 @@ public class Plant
 
     public Plant(
         Fingerprint uniqueMarker,
-        HyphaApex name,
+        HyphaeStrain primaryHyphae,
         IEnumerable<Cell> cells,
-        IEnumerable<Hypha>? associatedHyphae
+        List<HyphaeStrain>? associatedHyphae
         )
     {
         UniqueMarker = uniqueMarker;
-        Name = name;
+
+        if (primaryHyphae.Value.Last() is not HyphaApex)
+        {
+            throw new ArgumentException(
+                "HyphaeStrain must be a HyphaApex to increment version."
+            );
+        }
+
+        Name = primaryHyphae;
+
         Cells = cells.ToImmutableSortedDictionary(
             cell => cell.UniqueMarker,
             cell => cell
             );
 
-        AssociatedHyphae = associatedHyphae?.ToDictionary(
-            HyphaeHierarchy.AsString,
-            hypha => hypha
-            ) ?? new Dictionary<string, Hypha>();
+        AssociatedHyphae = associatedHyphae
+                           ?? new List<HyphaeStrain>();
+
     }
 
 
-    public static Plant Cultivate(
-        Cell seed, string? name, IEnumerable<Cell> cells, IEnumerable<Hypha>? associatedHyphae
+    /// <summary>
+    /// Does cultivate this plant into a new type of plant, by adding new cells,<br/>
+    /// while preserving a continuous naming convention (-v1 -v2 -v3 ... -vN).<br/>
+    /// <b>Does not put primaryHyphae in a Mycelium!</b>
+    /// </summary>
+    /// <param name="newCells"></param>
+    /// <returns>Newly Cultivated Plant</returns>
+    public Plant CultivateWith(
+        IEnumerable<Cell> newCells,
+        HyphaeStrain? primaryHyphae, List<HyphaeStrain>? additionalAssociatedHyphae,
+        Fingerprint? uniqueMarker
         )
     {
-        var uniqueMarker = new Fingerprint();
+        uniqueMarker ??= new Fingerprint();
+        primaryHyphae ??= PlantNameHelper.IncrementVersion(Name);
+
         return new Plant(
             uniqueMarker,
-            new HyphaApex(name ?? uniqueMarker.ToString()),
-            cells, associatedHyphae
+            primaryHyphae,
+            newCells, 
+            additionalAssociatedHyphae
         );
     }
 
+}
+
+public static class PlantNameHelper
+{
+    private static readonly char VersionSymbol = 'v';
+    private static readonly Regex VersionRegex = new(
+        $@"{VersionSymbol}(\d+)$", RegexOptions.Compiled
+        );  // does check vor 'v1' or 'v134674'
+
+
+    /// <summary>
+    /// Checks if the name ends with a version pattern (-vN) and extracts the version number if present.
+    /// </summary>
+    /// <returns>The extracted version number if present, otherwise null.</returns>
+    public static int ExtractVersion(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            return 0;
+        }
+
+        var match = VersionRegex.Match(name);
+        return match.Success ? int.Parse(match.Groups[1].Value) : 0;
+    }
+
+    public static HyphaeStrain IncrementVersion(HyphaeStrain strain)
+    {
+        var version = 0;
+
+        if (strain.Value.Last() is HyphaApex)
+        {
+            var versionHypha = (HyphaApex)strain.Value.Last();
+            version = ExtractVersion(versionHypha.Value.ToString() ?? string.Empty);
+        }
+
+        var newVersion = version + 1;
+        var newTail = new HyphaApex(
+            $"{VersionSymbol}{newVersion}"
+        );
+
+        var newStrain = strain.Value
+            .SkipLast(1)
+            .Append(newTail)
+            .ToImmutableArray();
+
+        return new HyphaeStrain(newStrain);
+    }
 }
