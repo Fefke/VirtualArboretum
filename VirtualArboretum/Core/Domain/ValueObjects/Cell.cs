401e585da99e6735d4e3808b9ac68805560b039f
FileX <frag.mich.hier@gmail.com>
2025-04-22 19:25:37 +0200

    Refactor Cell class to use CellType instead of ContentType and add CellType class for better type management

--- VirtualArboretum/Core/Domain/ValueObjects/Cell.cs
+++ VirtualArboretum/Core/Domain/ValueObjects/Cell.cs
@@ -1,9 +1,4 @@
-﻿using System.Net;
-using System.Net.Mime;
-using System.Runtime.InteropServices;
-using System.Text;
-
-namespace VirtualArboretum.Core.Domain.ValueObjects;
+﻿namespace VirtualArboretum.Core.Domain.ValueObjects;
 
 /// <summary>
 /// A single cell, just like Jupyter-Notebook cells, can contain any kind of data,
@@ -14,13 +9,25 @@ public class Cell
 
     public readonly Fingerprint UniqueMarker;
 
-    public ContentType Type { get; }
+    public CellType Type { get; }
     public ReadOnlyMemory<byte> Organell { get; }
 
-    public Cell(ReadOnlyMemory<byte> organell, ContentType type, Fingerprint uniqueMarker)
+    public Cell(ReadOnlyMemory<byte> organell, CellType type, Fingerprint uniqueMarker)
     {
         UniqueMarker = uniqueMarker;
-        Type = type ?? throw new ArgumentNullException(nameof(type));
-        Organell = organell; // ReadOnlyMemory<byte> ist ein Struct, keine Null-Prüfung nötig
+        Type = type;
+        Organell = organell;
     }
+
+    public Cell(ReadOnlyMemory<byte> organell, Fingerprint uniqueMarker)
+        : this(organell, new CellType("application/octet-stream"), uniqueMarker)
+    { }
+
+    public Cell(ReadOnlyMemory<byte> organell, CellType type)
+        : this(organell, type, new Fingerprint())
+    { }
+
+    public Cell(ReadOnlyMemory<byte> organell)
+        : this(organell, new CellType("application/octet-stream"), new Fingerprint())
+    { }
 }
---
+++ VirtualArboretum/Core/Domain/ValueObjects/CellType.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Mime;
+
+namespace VirtualArboretum.Core.Domain.ValueObjects;
+
+public class CellType : ContentType
+{
+    public CellType(string name) : base(name)
+    { }
+
+    public bool IsOfficiallySupported() => this.Name switch
+    {
+        "application/json" => true,
+        "application/octet-stream" => true,
+        _ => false
+    };
+}
