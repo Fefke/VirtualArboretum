8d22723d0b4d8dc3f6b4aecfdeed55e2d40da9b2
FileX <frag.mich.hier@gmail.com>
2025-04-22 22:52:57 +0200

    Add Garden class to represent a namespace for plants and manage their associations

---
+++ VirtualArboretum/Core/Domain/Entities/Garden.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Data.Common;
+using VirtualArboretum.Core.Domain.AggregateRoots;
+using VirtualArboretum.Core.Domain.ValueObjects;
+
+namespace VirtualArboretum.Core.Domain.Entities;
+
+/// <summary>
+/// A garden does represent a namespace for a variety of plants,
+/// a local mycelium
+/// </summary>
+public class Garden
+{
+    /// <summary>
+    /// <i>All child plants are automatically being associated with this location.</i>
+    /// </summary>
+    public HyphaeStrain PrimaryLocation { get; } // does directly reflect into DirectoryInfo Location.
+
+    private readonly ConcurrentDictionary<Fingerprint, Plant> _plants;
+
+    public Garden(HyphaeStrain primaryLocation, IList<Plant> plants)
+    {
+        PrimaryLocation = primaryLocation;
+        _plants = new(
+            concurrencyLevel: -1,
+            capacity: plants.Count
+            );
+
+        foreach (var plant in plants.AsParallel())
+        {
+            AddPlant(plant);
+        }
+    }
+
+
+    public Plant AddPlant(Plant plant)
+    {
+        
+        plant.AssociateWith(PrimaryLocation);
+
+        _plants.AddOrUpdate(
+            plant.UniqueMarker,
+            plant,
+            (_, presentPlant) => plant);
+
+        return plant;
+    }
+    
+    public Plant? GetPlant(Fingerprint fingerprint)
+    {
+        return _plants.GetValueOrDefault(fingerprint);
+    }
+
+}
