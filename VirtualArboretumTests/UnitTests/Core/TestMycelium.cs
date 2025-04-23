d5dc51f728fcf927b7adf60d545e6d5f24b465e8
FileX <frag.mich.hier@gmail.com>
2025-04-23 22:30:13 +0200

    Add Mycelium unit test class template

---
+++ VirtualArboretumTests/UnitTests/Core/TestMycelium.cs
@@ -0,0 +1,11 @@
+namespace VirtualArboretumTests.UnitTests.Core;
+
+[TestClass]
+public class UnitTest4
+{
+    [TestMethod]
+    public void Test1()
+    {
+        // TODO: Primarily Test Mycelium Query Methods once implemented...
+    }
+}
