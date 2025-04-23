f6bf5bb5bfecfb9850832366f654df6ab54bd657
FileX <frag.mich.hier@gmail.com>
2025-04-23 22:29:52 +0200

    Refactor Arboretum class to remove unused primaryHyphae parameter and streamline Mycelium initialization

--- VirtualArboretum/Core/Domain/AggregateRoots/Arboretum.cs
+++ VirtualArboretum/Core/Domain/AggregateRoots/Arboretum.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Collections.ObjectModel;
 using VirtualArboretum.Core.Domain.Entities;
 using VirtualArboretum.Core.Domain.ValueObjects;
 
@@ -7,38 +6,62 @@ namespace VirtualArboretum.Core.Domain.AggregateRoots;
 
 public class Arboretum
 {
-    // TODO: Does manage multiple Gardens...
-    //  - Does read in provided user-config (req. config-class)
+    // TODO: Does read in provided user-config (req. config-class)
 
-
-    private HyphaeStrain primaryHyphae; // Can be home-dir or user-specified-dir i.R.
+    //private HyphaeStrain primaryHyphae; // Can be home-dir or user-specified-dir i.R.
 
     private readonly ConcurrentDictionary<Fingerprint, Garden> _gardens;
 
     private readonly Mycelium _mycelium;
 
-    public Arboretum(IEnumerable<Garden> gardens, HyphaeStrain primaryHyphae)
+    public Arboretum(IEnumerable<Garden> gardens)
     {
         _gardens = new(gardens.ToDictionary(
             garden => garden.UniqueMarker
             ));
 
-        this.primaryHyphae = primaryHyphae;
-
-        InitializeMycelium();
+        _mycelium = new Mycelium();
+        InitializeMyceliumWith(_gardens.Values);
     }
 
-    private void InitializeMycelium()
+    private void InitializeMyceliumWith(ICollection<Garden> gardens)
     {
-        
-        /*
-         * TODO:
-         *  1. Read in all Hyphae from all Gardens.
-         *  2. Create a Mycelium for all Hyphae.
-         *  3. Associate all HyphaeStrains in Mycelium.
-         *  4. Associate all HyphaeStrains with their corresponding Fingerprints.
-         */
+
+        // Associate all Gardens, Plants and their hyphae
+        gardens.AsParallel().ForAll(
+            garden =>
+            {
+                _mycelium.AssociateWith(
+                    garden.PrimaryLocation, garden.UniqueMarker
+                );
+
+                // Associate all Plants and their hyphae
+                garden.GetPlants().AsParallel()
+                    .ForAll(Mycorrhizate);
+            });
+    }
+
+    /// <summary>
+    /// Does associate the plant with the mycelium by all the plants HyphaeStrains (including Name).
+    /// </summary>
+    /// TODO: This Method might be more appropriate in Garden, with local Mycelium. Not for now tho.
+    public void Mycorrhizate(Plant plant)
+    {
+        // Plants identity
+        _mycelium.AssociateWith(
+            plant.Name, plant.UniqueMarker
+        );
+        // Plants associations
+        _mycelium.AssociateWith(
+            plant.AssociatedHyphae, plant.UniqueMarker
+        );
     }
 
 
+    public Garden? ViewGarden(Fingerprint uniqueMarker)
+    {
+        _gardens.TryGetValue(uniqueMarker, out var garden);
+        return garden;
+    }
 }
+
