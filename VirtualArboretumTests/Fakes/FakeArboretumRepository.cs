884a5f1e3dcf60492ae496d4fc3c4ed8ad83967e
FileX <frag.mich.hier@gmail.com>
2025-04-26 19:07:57 +0200

    Add fake repository classes for testing and update project file to include unit test folder

---
+++ VirtualArboretumTests/Fakes/FakeArboretumRepository.cs
@@ -0,0 +1,28 @@
+﻿using VirtualArboretum.Core.Application.Interfaces;
+using VirtualArboretum.Core.Domain.AggregateRoots;
+using VirtualArboretum.Core.Domain.ValueObjects;
+
+namespace VirtualArboretumTests.Fakes;
+
+public class FakeArboretumRepository : IArboretumRepository
+{
+    public Task<Arboretum?> GetByFingerprintAsync(Fingerprint id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Arboretum?> GetByPrimaryHyphaeAsync(HyphaeStrain strain)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Arboretum candidate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Arboretum candidate)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ VirtualArboretumTests/Fakes/FakeGardenRepository.cs
@@ -0,0 +1,29 @@
+﻿using VirtualArboretum.Core.Application.Interfaces;
+using VirtualArboretum.Core.Domain.AggregateRoots;
+using VirtualArboretum.Core.Domain.Entities;
+using VirtualArboretum.Core.Domain.ValueObjects;
+
+namespace VirtualArboretumTests.Fakes;
+
+public class FakeGardenRepository : IGardenRepository
+{
+    public Task<Garden?> GetByFingerprintAsync(Fingerprint id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Garden?> GetByPrimaryHyphaeAsync(HyphaeStrain strain)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Garden candidate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Garden candidate)
+    {
+        throw new NotImplementedException();
+    }
+}
